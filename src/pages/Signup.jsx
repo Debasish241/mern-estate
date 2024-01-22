f35a248360ed0d863593ab20cec96c41c1308050
Debasish <ddebnath241@gmail.com>
2024-01-23 01:08:09 +0530

    create pages and route

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Signin from "./Signin";
+
+export default function Signup() {
+  return (
+    <h1>Signup</h1>
+  );
+}
