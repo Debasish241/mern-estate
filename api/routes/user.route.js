f279ce0e312f00217c0562c2b9d228f7d301ace8
Debasish <ddebnath241@gmail.com>
2024-01-27 18:20:10 +0530

    create user update api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,23 @@
-// user.route.js
+// import express from 'express';
+// import { test, updateUser } from '../controllers/user.controller.js';
+// import { verifyToken } from '../utils/verifyUser.js';
+// const router = express.Router();
+
+// router.get('/test', test);
+// router.post('/update/:id',verifyToken, updateUser)
+
+// export default router;
+
 
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser} from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
+
 const router = express.Router();
 
 router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser)
+
 
 export default router;
